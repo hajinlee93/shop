bd15acf9a273ede1ce01206404ff2706b5b6f949
programtaeyeon <kty2235@naver.com>
2024-05-28 20:42:56 +0900

    VO 주석 달기

--- refresh/src/main/java/login/vo/LoginVO.java
+++ refresh/src/main/java/login/vo/LoginVO.java
@@ -4,19 +4,19 @@ import lombok.Data;
 
 @Data
 public class LoginVO {
-	String userNo			;
-	String userId			;
-	String userPw			;
-	String userName			;
-	String gender			;
-	String userJumin		;
-	String mobileCarrier	;
-	String phone			;
-	String email			;
-	String zipcode			;
-	String address			;
-	String detailed_addres  ;
-	String rgstTime		    ;
-	String udtTime	        ;
-	String gubn				;
+	String userNo			;	//고객번호
+	String userId			;	//고객아이디
+	String userPw			;	//고객비밀번호
+	String userName			;	//고객이름
+	String gender			;	//성별
+	String userJumin		;	//주민번호
+	String mobileCarrier	;	//통신사
+	String phone			;	//휴대폰번호
+	String email			;	//이메일
+	String zipcode			;	//우편번호
+	String address			;	//주소
+	String detailed_addres  ;	//상세주소
+	String rgstTime		    ;	//등록시간
+	String udtTime	        ;	//수정시간
+	String gubn				;	//등급구분
 }
